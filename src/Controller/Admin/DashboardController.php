47754307e6027de1dfceb2cacbd557285acadd48
Walid <walid.ftaymia@gmail.com>
2023-08-19 13:02:58 +0200

    configuration administrative part

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\Menu;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -40,23 +41,43 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToRoute('Aller sur le site', 'fa fa-undo', 'app_home');
-        yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
-            MenuItem::linkToCrud('Tous les articles', 'fas fa-newspaper', Article::class),
-            MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class)
-        ]);
 
-        yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
-            MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class)
-                ->setQueryParameter('submenuIndex', 0),
-            MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class)
-                ->setQueryParameter('submenuIndex', 1),
-            MenuItem::linkToCrud('Liens personnalisés', 'fas fa-link', Menu::class)
-                ->setQueryParameter('submenuIndex', 2),
-            MenuItem::linkToCrud('Catégories', 'fab fa-delicious', Menu::class)
-                ->setQueryParameter('submenuIndex', 3),
-        ]);
-        yield MenuItem::linkToCrud('commentaire', 'fas fa-comment', Comment::class);
+        
+        
+        if ($this->isGranted('ROLE_ADMIN')){
+            yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
+                MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class)
+                    ->setQueryParameter('submenuIndex', 0),
+                MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class)
+                    ->setQueryParameter('submenuIndex', 1),
+                MenuItem::linkToCrud('Liens personnalisés', 'fas fa-link', Menu::class)
+                    ->setQueryParameter('submenuIndex', 2),
+                MenuItem::linkToCrud('Catégories', 'fab fa-delicious', Menu::class)
+                    ->setQueryParameter('submenuIndex', 3),
+            ]);
+        }
+
+        if ($this->isGranted('ROLE_AUTHOR')){
+            yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
+                MenuItem::linkToCrud('Tous les articles', 'fas fa-newspaper', Article::class),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class)
+            ]);
+        }
+        
+        if ($this->isGranted('ROLE_ADMIN')){
+            yield MenuItem::linkToCrud('Commentaire', 'fas fa-comment', Comment::class);
+
+            yield MenuItem::subMenu('Comptes', 'fas fa-user')->setSubItems([
+                MenuItem::linkToCrud('Tous les comptes', 'fas fa-user-friends', User::class)
+                    ->setQueryParameter('submenuIndex', 0),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', User::class)
+                    ->setAction(Crud::PAGE_NEW)
+                    ->setQueryParameter('submenuIndex', 1),
+             
+            ]);
+        }
+       
     }
 
 }
--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Entity\User;
 use App\Repository\ArticleRepository;
 use App\Repository\CommentRepository;
 use App\Repository\UserRepository;
@@ -12,12 +13,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @method User getUser()
+ */
 class CommentController extends AbstractController
 {
     #[Route('/ajax/comments', name: 'comment_add')]
     public function add(Request $request, ArticleRepository $articleRepo, CommentRepository $commentRepo, EntityManagerInterface $em, UserRepository $userRepo): Response
     {
         $commentData = $request->request->all('comment');
+       
 
         if (!$this->isCsrfTokenValid('comment-add', $commentData['_token'])){
             return $this->json([
@@ -33,9 +38,17 @@ class CommentController extends AbstractController
             ], Response::HTTP_BAD_REQUEST);
         }
 
+        $user = $this->getUser();
+
+        if (!$user){
+            return $this->json([
+                'code' => "USER_NOT_AUTHENTICATED_FULLY"
+            ], Response::HTTP_BAD_REQUEST);
+        }
         $comment = new Comment($article);
+        
         $comment->setContent($commentData['content']);
-        $comment->setUser($userRepo->findOneBy(['id' => 1]));
+        $comment->setUser($user);
         $comment->setCreatedAt(new \DateTime());
 
         $em->persist($comment);
@@ -48,7 +61,7 @@ class CommentController extends AbstractController
         return $this->json([
             'code' => 'COMMENT_ADDED_SUCCESSFULLY',
             'message' => $html,
-            'numberOfComments' => $commentRepo->count(['artcile' => $article])
+            'numberOfComments' => $commentRepo->count(['article' => $article])
         ]);
         
     }
